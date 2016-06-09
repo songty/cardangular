0d33d3376f27175072474a309eaa1f61816fb9e0
Tian Song <tian.y.song@gmail.com>
2016-06-09 16:02:12 -0700

    setting up project with boilerplates.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cardangularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cardangularApp
+ */
+angular.module('cardangularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cardangularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cardangularApp
+ */
+angular.module('cardangularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
